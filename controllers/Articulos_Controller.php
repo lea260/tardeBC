ad41c3d5ea9b07fa07ede86f68c111ad78673c0f
Augusto Picardo <auguselo77@gmail.com>
2022-06-04 14:23:36 -0300

    mod Articulos_Controller.php

--- controllers/Articulos_Controller.php
+++ controllers/Articulos_Controller.php
@@ -1,1 +1,24 @@
-<?php ; ?>
+<?php 
+
+require_once 'vendor/autoload.php';
+require_once 'auth/Auth.php';
+
+class Articulos_Controller extends Controller{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->view->mensaje = "";
+        $this->view->resultadoLogin = "";
+    }
+
+    //base+login
+    public function render()
+    {
+        //$alumnos = $this->model->get();
+        $this->view->alumnos = "cargado";
+        $this->view->render('login/index');
+    }
+
+  }
+
+; ?>
