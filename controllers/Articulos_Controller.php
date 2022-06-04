6ced175e983fd82deb781b53278e9af4cf34d0ac
Maticerde <Maticerde>
2022-06-04 14:18:43 -0300

    modifique articulos controlador

--- controllers/Articulos_Controller.php
+++ controllers/Articulos_Controller.php
@@ -1,4 +1,22 @@
 <?php
+require_once 'vendor/autoload.php';
+require_once 'auth/Auth.php';
 
-j == hola;
-echo j;
+class Articulos_Controller extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->view->mensaje        = "";
+        $this->view->resultadoLogin = "";
+    }
+
+    //base+login
+    public function render()
+    {
+        //$alumnos = $this->model->get();
+        $this->view->alumnos = "cargado";
+        $this->view->render('login/index');
+    }
+
+}
