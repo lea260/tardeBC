ae473ad70e102b74552ad5e2d14709a94e623059
BautistaCanobra <bautijuan06@gmail.com>
2022-06-04 14:37:53 -0300

    subi trabajo

---
+++ controllers/Articulos_Controllers.php
@@ -0,0 +1,26 @@
+<?php
+
+class Login_Controller extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->view->mensaje        = "";
+        $this->view->resultadoLogin = "";
+    }
+
+    //base+login
+    public function render()
+    {
+        //$alumnos = $this->model->get();
+        $this->view->alumnos = "cargado";
+        $this->view->render('login/index');
+    }
+ public function listar()
+    {
+        //$alumnos = $this->model->get();
+        $this->view->mensaje = "cargado";
+        $this->view->render('articulos/listar');
+    }
+    
+}
