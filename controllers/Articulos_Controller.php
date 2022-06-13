a6277e913d2a1d2e18965bed487d14e9ae379b7f
TexasXThomas <liantorrio@gmail.com>
2022-06-13 15:48:19 -0300

    agregue formulario de crear, es nuevo.php

--- controllers/Articulos_Controller.php
+++ controllers/Articulos_Controller.php
@@ -1,26 +1,24 @@
 <?php
 
-
 class Articulos_Controller extends Controller
 {
-    public function __construct ()
+    public function __construct()
     {
         parent::__construct();
         $this->view->mensaje        = "";
         $this->view->resultadoLogin = "";
     }
 
-    public function render()  {
-    $this->view->mensaje = "cargado";
-    $this->view->render('articulos/index');
+    public function render()
+    {
+        $this->view->mensaje = "cargado";
+        $this->view->render('articulos/index');
 
-        
     }
-      
-  
+
     //base+login
 
- public function listar($param = null)
+    public function listar($param = null)
     {
 
         //obtiene todos los articulos
@@ -29,6 +27,51 @@ class Articulos_Controller extends Controller
         $this->view->lista = $articulos;
         //lista los articulos
         $this->view->render('articulos/listar');
-        $arr = array();
+        $arr = [];
     }
-       }
+
+    public function nuevo($param = null)
+    {
+
+        //obtiene todos los articulos
+        //$articulos = $this->model->listar();
+        //lo asigna a la varible articulos
+        //$this->view->lista = $articulos;
+        //lista los articulos
+        $this->view->render('articulos/nuevo');
+        //$arr = [];
+    }
+
+    public function crear()
+    {
+
+        //obtengo los datos de la peticion http, post body
+        $json = file_get_contents('php://input');
+        //convierto en un array asociativo de php
+        $obj = json_decode($json);
+
+        $articulo              = new Articulo();
+        $articulo->codigo      = $obj->codigo;
+        $articulo->descripcion = $obj->descripcion;
+        $articulo->precio      = $obj->precio;
+        $articulo->fecha       = $obj->fecha;
+        //array_push($listaArticulos, $articulo);
+        //$items[] = $item;
+
+        $resultado = $this->model->crear($articulo);
+        //$articulo->id = $obj->id;
+        //$articulo->nombre = $obj->nombre;
+        //$articulos = $this->model->get();
+        //$this->view->articulos = json_encode($articulos);
+        //$listaObjetos = json_encode($listaArticulos);
+
+        $respuesta = [
+            "ArituloId" => $resultado,
+        ];
+        $this->view->respuesta = json_encode($respuesta);
+
+        $this->view->render('api260260/articulos/crearm');
+        //var_dump($this);
+        //var_dump($this->view);
+    } //end crear
+}
