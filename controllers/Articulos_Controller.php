82f03cf799858bc3f911f8ec82ffa98efcb446e0
leandro <lcalvetti1@gmail.com>
2022-06-13 15:48:49 -0300

    agregue formulario de crear articulo

--- controllers/Articulos_Controller.php
+++ controllers/Articulos_Controller.php
@@ -16,6 +16,14 @@ class Articulos_Controller extends Controller
         $this->view->mensaje = "cargado";
         $this->view->render('articulos/index');
     }
+
+    public function nuevo($param = null)
+    {
+
+        //lista los articulos
+        $this->view->render('articulos/nuevo');
+
+    } //end listar
     public function listar($param = null)
     {
 
@@ -26,6 +34,37 @@ class Articulos_Controller extends Controller
         //lista los articulos
         $this->view->render('articulos/listar');
         $arr = [];
-    }
+    } //end listar
+
+    public function crear()
+    {
+        //obtengo los datos de la peticion http, post body
+        $json = file_get_contents('php://input');
+        //convierto en un array asociativo de php
+        $obj                   = json_decode($json);
+        $articulo              = new Articulo();
+        $articulo->codigo      = $obj->codigo;
+        $articulo->descripcion = $obj->descripcion;
+        $articulo->precio      = $obj->precio;
+        $articulo->fecha       = $obj->fecha;
+        //array_push($listaArticulos, $articulo);
+        //$items[] = $item;
+
+        $resultado = $this->model->crear($articulo);
+        //$articulo->id = $obj->id;
+        //$articulo->nombre = $obj->nombre;
+        //$articulos = $this->model->get();
+        //$this->view->articulos = json_encode($articulos);
+        //$listaObjetos = json_encode($listaArticulos);
+
+        $respuesta = [
+            "ArituloId" => $resultado,
+        ];
+        $this->view->respuesta = json_encode($respuesta);
+
+        $this->view->render('api260260/articulos/crearm');
+        //var_dump($this);
+        //var_dump($this->view);
+    } //end crear
 
 }
