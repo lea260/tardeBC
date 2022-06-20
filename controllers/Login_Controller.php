acea88e83bcd0fdf1f197352b55a8438676c1367
Maticerde <Maticerde>
2022-06-20 16:11:21 -0300

    agregue cambios4

--- controllers/Login_Controller.php
+++ controllers/Login_Controller.php
@@ -1,13 +1,11 @@
 <?php
-require_once 'vendor/autoload.php';
-require_once 'auth/Auth.php';
 
 class Login_Controller extends Controller
 {
     public function __construct()
     {
         parent::__construct();
-        $this->view->mensaje = "";
+        $this->view->mensaje        = "";
         $this->view->resultadoLogin = "";
     }
 
@@ -21,8 +19,8 @@ class Login_Controller extends Controller
 
     public function ingresar()
     {
-        $nombre = $_POST['nombre'];
-        $pass = $_POST['pass'];
+        $nombre     = $_POST['nombre'];
+        $pass       = $_POST['pass'];
         $exitoLogin = $this->model->ingresar($nombre, $pass);
         if ($exitoLogin) {
             $token = Auth::SignIn([
@@ -30,10 +28,10 @@ class Login_Controller extends Controller
                 'name' => $nombre,
                 'role' => 'cliente',
             ]);
-            $this->view->token = $token;
+            $this->view->token        = $token;
             $_SESSION["estalogueado"] = true;
-            $_SESSION["nombre"] = $nombre;
-            $_SESSION["rol"] = "cliente";
+            $_SESSION["nombre"]       = $nombre;
+            $_SESSION["rol"]          = "cliente";
             $this->view->render('login/ingresar');
         } else {
             $this->view->resultadoLogin = "usuario o contraseña incorrectos";
@@ -48,6 +46,14 @@ class Login_Controller extends Controller
         unset($_SESSION["nombre"]);
         session_destroy();
         $this->view->render('index/index');
+
+    }
+    public function login()
+    {
+        //$_SESSION["estalogueado"] = false;
+        echo "ingresar";
+
+        //$this->view->render('index/index');
 
     }
 }
