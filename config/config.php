3d49e0d64fb8133a05df6ce62a628295f842d8a3
BautistaCanobra <bautijuan06@gmail.com>
2022-06-20 16:19:47 -0300

    dd

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+//define('URL', 'http://localhost/mvc/');
+define('URL', 'http://' . $_SERVER['HTTP_HOST'] . '/tardeBCb/');
+
+//conexion a la base de datos
+define('HOST', 'localhost');
+define('PORT', '3307');
+define('DB', 'tardeBCv01');
+define('USER', 'usuario03');
+define('PASSWORD', "usuario03");
+define('CHARSET', 'utf8mb4');
---
+++ models/Login_Model.php
@@ -0,0 +1,13 @@
+<?php
+
+#require_once 'entidades/alumno.php';
+require_once 'entidades/articulos.php';
+
+class Login_Model extends Model
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
