2294ae5107eed4df38f6fb0de160c5cc1d6afefc
clavijo <clavijo>
2022-06-20 14:53:16 -0300

    agregue cambios a Articulos_Controller.php

--- controllers/Articulos_Controller.php
+++ controllers/Articulos_Controller.php
@@ -13,14 +13,95 @@ class Articulos_Controller extends Controller
     public function render()
     {
         //$alumnos = $this->model->get();
-        $this->view->alumnos = "cargado";
-        $this->view->render('login/index');
-    }
-    public function listar()
-    {
-        //$alumnos = $this->model->get();
         $this->view->mensaje = "cargado";
+        $this->view->render('articulos/index');
+    }
+
+    public function nuevo($param = null)
+    {
+
+        //lista los articulos
+        $this->view->render('articulos/nuevo');
+
+    } //end listar
+    public function listar($param = null)
+    {
+
+        //obtiene todos los articulos
+        $articulos = $this->model->listar();
+        //lo asigna a la varible articulos
+        $this->view->listar = $articulos;
+        //lista los articulos
         $this->view->render('articulos/listar');
+        $arr = [];
+    } //end listar
+
+    public function crear()
+    {
+
+        $codigo                = $_POST['codigo'];
+        $descripcion           = $_POST['descripcion'];
+        $precio                = $_POST['precio'];
+        $fecha                 = $_POST['fecha'];
+        $articulo              = new Articulo();
+        $articulo->codigo      = $codigo;
+        $articulo->descripcion = $descripcion;
+        $articulo->precio      = $precio;
+        $articulo->fecha       = $fecha;
+
+        $resultado             = $this->model->crear($articulo);
+        $this->view->resultado = $resultado;
+
+        $this->view->render('articulos/crear');
+
+    } //end crear
+
+    public function actualizar()
+    {
+
+        $id                    = $_POST['idaux'];
+        $codigo                = $_POST['codigo'];
+        $descripcion           = $_POST['descripcion'];
+        $precio                = $_POST['precio'];
+        $fecha                 = $_POST['fecha'];
+        $articulo              = new Articulo();
+        $articulo->id          = $id;
+        $articulo->codigo      = $codigo;
+        $articulo->descripcion = $descripcion;
+        $articulo->precio      = $precio;
+        $articulo->fecha       = $fecha;
+
+        $resultado             = $this->model->actualizar($articulo);
+        $this->view->resultado = $articulo->id;
+
+        $this->view->render('articulos/actualizar');
+
+    } //end crear
+
+    public function verArticulo($param = null)
+    {
+        $idArticulo = $param[0];
+        $articulo   = $this->model->verArticulo($idArticulo);
+
+        $_SESSION["id_articulo"] = $idArticulo;
+
+        $this->view->articulo = $articulo;
+
+        $this->view->render('articulos/verArticulo');
+    }
+
+    public function eliminar($param = null)
+    {
+        $id = $_POST['idaux'];
+
+        if ($this->model->eliminar($id)) {
+            $mensaje = "Articulo eliminado correctamente";
+            //$this->view->mensaje = "Alumno eliminado correctamente";
+        } else {
+            $mensaje = "No se pudo eliminar al alumno";
+            //$this->view->mensaje = "No se pudo eliminar al alumno";
+        }
+        echo $mensaje;
     }
 
 }
