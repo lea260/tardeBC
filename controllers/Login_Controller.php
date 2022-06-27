3d94d6543cee1438190af61b7519ce63956d3c18
mendoza <mendozagonzalo585@gmail.com>
2022-06-27 14:43:15 -0300

    agregue contenido a login_controller.php y a articulos02_model.php

--- controllers/Login_Controller.php
+++ controllers/Login_Controller.php
@@ -7,10 +7,13 @@ class Login_Controller extends Controller
     public function __construct()
     {
         parent::__construct();
-        $this->view->mensaje = "";
+        $this->view->mensaje        = "";
         $this->view->resultadoLogin = "";
     }
-
+    public function login()
+    {
+        echo "ingresar";
+    }
     //base+login
     public function render()
     {
@@ -21,8 +24,8 @@ class Login_Controller extends Controller
 
     public function ingresar()
     {
-        $nombre = $_POST['nombre'];
-        $pass = $_POST['pass'];
+        $nombre     = $_POST['nombre'];
+        $pass       = $_POST['pass'];
         $exitoLogin = $this->model->ingresar($nombre, $pass);
         if ($exitoLogin) {
             $token = Auth::SignIn([
@@ -30,10 +33,10 @@ class Login_Controller extends Controller
                 'name' => $nombre,
                 'role' => 'cliente',
             ]);
-            $this->view->token = $token;
+            $this->view->token        = $token;
             $_SESSION["estalogueado"] = true;
-            $_SESSION["nombre"] = $nombre;
-            $_SESSION["rol"] = "cliente";
+            $_SESSION["nombre"]       = $nombre;
+            $_SESSION["rol"]          = "cliente";
             $this->view->render('login/ingresar');
         } else {
             $this->view->resultadoLogin = "usuario o contraseña incorrectos";
