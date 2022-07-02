daf87445036020d9a72521da8d623b72fb4dd6d0
mendoza <mendozagonzalo585@gmail.com>
2022-07-02 13:30:02 -0300

    arregle Login_Controller.php y elimine config.php por demasiados errores

--- controllers/Login_Controller.php
+++ controllers/Login_Controller.php
@@ -1,6 +1,7 @@
 <?php
-#require_once 'vendor/autoload.php';
-#require_once 'auth/Auth.php';
+// require_once 'vendor/autoload.php';
+// require_once 'auth/Auth.php';
+require_once 'entidades/usuario.php';
 
 class Login_Controller extends Controller
 {
@@ -11,7 +12,6 @@ class Login_Controller extends Controller
         $this->view->resultadoLogin = "";
     }
 
-    //base+login
     public function render()
     {
         //$alumnos = $this->model->get();
@@ -19,45 +19,27 @@ class Login_Controller extends Controller
         $this->view->render('login/index');
     }
 
-    public function ingresar()
+    public function login()
     {
-        $nombre     = $_POST['nombre'];
-        $pass       = $_POST['pass'];
-        $exitoLogin = $this->model->ingresar($nombre, $pass);
-        if ($exitoLogin) {
-            $token = Auth::SignIn([
-                'id' => 1,
-                'name' => $nombre,
-                'role' => 'cliente',
-            ]);
-            $this->view->token        = $token;
-            $_SESSION["estalogueado"] = true;
-            $_SESSION["nombre"]       = $nombre;
-            $_SESSION["rol"]          = "cliente";
-            $this->view->render('login/ingresar');
-        } else {
-            $this->view->resultadoLogin = "usuario o contraseña incorrectos";
-            $this->view->render('login/index');
-        }
-
+        $this->view->render('login/index');
     }
-    public function salir()
+    public function signin()
     {
-        //$_SESSION["estalogueado"] = false;
-        unset($_SESSION["estalogueado"]);
-        unset($_SESSION["nombre"]);
-        session_destroy();
-        $this->view->render('index/index');
+        $user           = new Usuario();
+        $user->email    = $_POST['email'];
+        $user->password = $_POST['password'];
+        $id             = $this->model->signin($user);
+        $_SESSION['id'] = serialize($id);
     }
-
     public function test()
     {
-        //$_SESSION["estalogueado"] = false;
-        $pwd                = '1234';
-        $hash               = password_hash($pwd, PASSWORD_BCRYPT, ['cost' => 10]);
-        $this->view->hash   = $hash;
-        $result             = password_verify($pwd, $hash);
-        $this->view->result = $result;
+        $pass             = "1234567890";
+        $hash             = password_hash($pass, PASSWORD_BCRYPT, ['cost' => 10]);
+        $ret              = password_verify($pass, $hash);
+        $this->view->hash = "Hash es: " . $hash;
+        $this->view->ret  = "devolucion es: " . $ret;
         $this->view->render('login/test');
+
     }
+
 }
