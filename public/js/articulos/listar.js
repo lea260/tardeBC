b95c98627a313de2a36ba519e84bd0aaeeb0ad70
FedeDini <FedeDini>
2022-08-15 15:50:01 -0300

    arregle errores

--- public/js/articulos/listar.js
+++ public/js/articulos/listar.js
@@ -1,37 +1,37 @@
-(function ($, param) {
+(function ($) {
   $(document).ready(function () {
-       var  $listaArticulos  =  [ ] ;
-    let  url  =  $ ( "#url" ) . valor ( ) ;
-    let  urlReq  =  url  +  "apiarticulos/listar" ;
+    var $lista = [];
+    let url = $("#url").val();
+    let urlReq = url + "apiarticulos/listar";
     //console.log("url: "+urlReq);
-    //consola.log(parámetro);
-    let  headers  =  {  "Content-Type" : "application/json;charset=utf-8"  } ;
-    dejar  datos  =  { } ;
-    $ . ajax ( {
-      url : urlReq ,
-      encabezados : encabezados ,
-      escriba : "POST" ,
-      datos : datos ,
-      tipo_de_datos : " json " ,
-    } )
-      . hecho ( función  ( datos )  {
-        // $listaArticulos = data.datos;
-        $lista  =  datos . lista;
-        consola_registro ( $lista ) ;
-      } )
-      . fail ( función  ( jqXHR ,  textStatus ,  errorThrown )  {
-        consola_registro (estado del texto) ;
-      } ) ;
-    $ ( ".btnAgregar" ) . cada ( función  (índice)  {
-      $ ( esto ) . en ( "clic" ,  función  () {
-        //consola.log("hola");
+    //console.log(param);
+    let headers = { "Content-Type": "application/json;charset=utf-8" };
+    let data = {};
+    $.ajax({
+      url: urlReq,
+      headers: headers,
+      type: "POST",
+      data: data,
+      dataType: "json",
+    })
+      .done(function (data) {
+        //$listaArticulos = data.datos;
+        $lista = data.lista;
+        console.log($lista);
+      })
+      .fail(function (jqXHR, textStatus, errorThrown) {
+        console.log(textStatus);
+      });
+
+    $(".btnAgregar").each(function (index) {
+      $(this).on("click", function () {
+        //console.log("hola");
         //let articuloId = this.dataset.articuloId;
-        let  articuloId  =  $ ( esto ) . datos ( "articuloId" ) ;
-        let  articuloDescripcion  =  $ ( este ) . data ( "articuloDescripcion" ) ;
-        let  articuloCodigo  =  $ ( este ) . datos ( "articuloCodigo" );
-        consolalog ( articuloId ) ;
-        
-      }); //end item click
-    }); //end item click items foreach
+        let articuloId = $(this).data("articuloId");
+        let articuloDescripcion = $(this).data("articuloDescripcion");
+        let articuloCodigo = $(this).data("articuloCodigo");
+        console.log(articuloId);
+      });
+    });
   }); //end ready
 })(jQuery);
