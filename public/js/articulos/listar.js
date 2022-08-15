d80cf1182011c27d08b3a93a6fddaa456d4f6ca5
Leandro <Leandro>
2022-08-15 19:10:07 -0300

    ultimas correcciones

--- public/js/articulos/listar.js
+++ public/js/articulos/listar.js
@@ -20,7 +20,7 @@
         //console.log($lista);
       })
       .fail(function (jqXHR, textStatus, errorThrown) {
-        console.log(textStatus);
+        //console.log(textStatus);
       });
 
     $(".btnAgregar").each(function (index) {
@@ -32,29 +32,33 @@
         let articuloDescripcion = $(this).data("articuloDescripcion");
         let articuloCodigo = $(this).data("articuloCodigo");
         //console.log(articuloId);
-        let articulo = $lista.find((art) => art.id == 5);
-        console.log(articulo);
+        let articulo = $lista.find((art) => art.id == $id);
+        //console.log(articulo);
         let cantidad = $("#art-" + $id).val();
-        console.log("cantidad:" + cantidad);
+        //console.log("cantidad:" + cantidad);
         let item = {
           id: $id,
           precio: articulo.precio,
           descripcion: articulo.descripcion,
           url: articulo.url,
           cantidad: cantidad,
+          codigo: articulo.codigo,
         };
 
         let carritoStr = localStorage.getItem("carrito");
         let carritoArr = [];
         if (carritoStr) {
+          //console.log(carritoStr);
+          console.log("entro if");
           carritoArr = JSON.parse(carritoStr);
           //agrego el elemento al carrito
           carritoArr.push(item);
+          localStorage.setItem("carrito", JSON.stringify(carritoArr));
         } else {
+          console.log("entro else");
           carritoArr.push(item);
           localStorage.setItem("carrito", JSON.stringify(carritoArr));
         }
-        console.log(item);
       });
     });
   }); //end ready
