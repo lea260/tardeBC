f50218e24d48193c85ce17b35e03e4051ad16c74
maticerde <alfonsotestabonino@gmail.com>
2022-08-15 14:51:25 -0300

    maticerde

--- public/js/articulos/listar.js
+++ public/js/articulos/listar.js
@@ -1,85 +1,37 @@
-(function ($, param) {
+(function ($) {
   $(document).ready(function () {
-    //alert('hola');
-    //console.log("funciona ver articulo");
-    let $listaArticulos = [];
+    var $lista = [];
     let url = $("#url").val();
-    let urlReq = url + "api260260articulos/listar";
-    console.log("url: " + urlReq);
-    console.log(param);
+    let urlReq = url + "apiarticulos/listar";
+    //console.log("url: "+urlReq);
+    //console.log(param);
     let headers = { "Content-Type": "application/json;charset=utf-8" };
     let data = {};
     $.ajax({
       url: urlReq,
       headers: headers,
-      type: "DELETE",
+      type: "POST",
       data: data,
+      dataType: "json",
     })
       .done(function (data) {
-        $listaArticulos = data.datos;
-        //console.log($listaArticulos);
+        //$listaArticulos = data.datos;
+        $lista = data.lista;
+        console.log($lista);
       })
       .fail(function (jqXHR, textStatus, errorThrown) {
-        serrorFunction();
+        console.log(textStatus);
       });
 
-    //asiganr la funcionalidad del carrito
-    const items = document.querySelectorAll(".btnAgregar");
-    items.forEach((item) => {
-      item.addEventListener("click", function () {
-        let articuloId = this.dataset.articuloId;
-        //console.log(articuloId);
-        let articulo = $listaArticulos.find(
-          (articulo) => articulo.id == articuloId
-        );
-        console.log(JSON.stringify(articulo));
-        carrito = JSON.parse(localStorage.getItem("carrito"));
-        console.log("carrito: " + carrito);
-        if (carrito == null) {
-          //inicilizo el carrito
-          //agrego el elememto al carrito
-          let cantidadAux = $("#art-" + articuloId).val();
-          let cantidad = 1;
-          if (cantidadAux >= 1) {
-            cantidad = cantidadAux;
-          }
-          console.log("cantidad:" + cantidad);
-          carrito = [];
-          console.log();
-          item = {
-            id: articulo.id,
-            precio: articulo.precio,
-            cantidad: cantidad,
-          };
-          carrito.push(item);
-          localStorage.setItem("carrito", JSON.stringify(carrito));
-        } else {
-          //ya tienen por lo menos un item
-          let cantidadAux = $("#art-" + articuloId).val();
-          let cantidad = 1;
-          if (cantidadAux >= 1) {
-            cantidad = cantidadAux;
-          }
-          console.log("cantidad:" + cantidad);
-          console.log();
-          item = {
-            id: articulo.id,
-            precio: articulo.precio,
-            cantidad: cantidad,
-          };
-          let itemCarrito = carrito.find(
-            (articulo) => articulo.id == articuloId
-          );
-          console.log("itemCarrito: " + itemCarrito);
-          if (itemCarrito == undefined) {
-            carrito.push(item);
-            localStorage.setItem("carrito", JSON.stringify(carrito));
-          }
-        }
-        //console.log("carrito: "+$carrito);
-        //localStorage.setItem("carrito", )
-        //$("#filaart-"+alumnoId).remove();
-      }); //end item click
-    }); //end item click items foreach
+    $(".btnAgregar").each(function (index) {
+      $(this).on("click", function () {
+        //console.log("hola");
+        //let articuloId = this.dataset.articuloId;
+        let articuloId = $(this).data("articuloId");
+        let articuloDescripcion = $(this).data("articuloDescripcion");
+        let articuloCodigo = $(this).data("articuloCodigo");
+        console.log(articuloId);
+      });
+    });
   }); //end ready
-})(jQuery, "hola mundo");
+})(jQuery);
