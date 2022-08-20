cea83a842f9537aaf41f91d600be231d26bd5cd0
Maticerde <Maticerde>
2022-08-20 14:15:56 -0300

    maticerde

---
+++ controllers/Carrito_Controller.php
@@ -0,0 +1,24 @@
+<?php
+
+class Carrito_Controller extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+    }
+
+    public function ver()
+    {
+        // $mensaje   = "hola desde la api";
+        // $lista     = $this->model->listar();
+        // $respuesta = [
+        //     "lista" => $lista,
+        //     "totalResultados" => count($lista),
+        //     "mensaje" => $mensaje,
+        // ];
+        // $this->view->respuesta = json_encode($respuesta);
+        $this->view->render("carrito/ver");
+    }
+
+}
