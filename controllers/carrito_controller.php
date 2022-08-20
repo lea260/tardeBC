286ec5f1a038aa8a9af5a66225ae49d33daa8884
LautaroMendez09 <lautaro_123mendez@hotmail.com>
2022-08-20 14:12:12 -0300

    agregue ver.js

--- controllers/carrito_controller.php
+++ controllers/carrito_controller.php
@@ -1,23 +1,19 @@
 <?php
-require_once "models/Articulos_Model";
-class carrito_Controller extends Controller
+require_once "models/Articulos_Model.php";
+class Carrito_Controller extends Controller
 {
-    public function __construct()
-    {
-        parent::__construct();
 
-    }
-
-    public function listar()
+    public function ver()
     {
-        $mensaje   = "hola desde la api";
+        /*$mensaje   = "hola desde la api";
         $lista     = $this->model->listar();
         $respuesta = [
-            "lista" => $lista,
-            "totalResultados" => count($lista),
-            "mensaje" => $mensaje,
+        "lista" => $lista,
+        "totalResultados" => count($lista),
+        "mensaje" => $mensaje,
         ];
-        $this->view->respuesta = json_encode($respuesta);
-        $this->view->render("apiarticulos/listar");
+        $this->view->respuesta = json_encode($respuesta);*/
+
+        $this->view->render("carrito/ver");
     }
 }
