dc71bfc2e6a07a28199281da5d53baf592b48204
Lucas Clavijo <fededini>
2022-08-22 16:10:57 -0300

    Modifique header.php y listar.js, mostrando el carrito y los articulos

--- public/js/articulos/listar.js
+++ public/js/articulos/listar.js
@@ -17,20 +17,48 @@
       .done(function (data) {
         //$listaArticulos = data.datos;
         $lista = data.lista;
-        console.log($lista);
+        //console.log($lista);
       })
       .fail(function (jqXHR, textStatus, errorThrown) {
-        console.log(textStatus);
+        //console.log(textStatus);
       });
 
     $(".btnAgregar").each(function (index) {
       $(this).on("click", function () {
         //console.log("hola");
         //let articuloId = this.dataset.articuloId;
-        let articuloId = $(this).data("articuloId");
+        //obtengo el id del articulo, mediante
+        let $id = $(this).data("articuloId");
         let articuloDescripcion = $(this).data("articuloDescripcion");
         let articuloCodigo = $(this).data("articuloCodigo");
-        console.log(articuloId);
+        //console.log(articuloId);
+        let articulo = $lista.find((art) => art.id == $id);
+        //console.log(articulo);
+        let cantidad = $("#art-" + $id).val();
+        //console.log("cantidad:" + cantidad);
+        let item = {
+          id: $id,
+          precio: articulo.precio,
+          descripcion: articulo.descripcion,
+          url: articulo.url,
+          cantidad: cantidad,
+          codigo: articulo.codigo,
+        };
+
+        let carritoStr = localStorage.getItem("carrito");
+        let carritoArr = [];
+        if (carritoStr) {
+          //console.log(carritoStr);
+          console.log("entro if");
+          carritoArr = JSON.parse(carritoStr);
+          //agrego el elemento al carrito
+          carritoArr.push(item);
+          localStorage.setItem("carrito", JSON.stringify(carritoArr));
+        } else {
+          console.log("entro else");
+          carritoArr.push(item);
+          localStorage.setItem("carrito", JSON.stringify(carritoArr));
+        }
       });
     });
   }); //end ready
--- views/header.php
+++ views/header.php
@@ -1,1 +1,10 @@
-<header>header</header>
+<header>
+  <div>
+    <a href="/tardeBC/articulos/listar#">Articulos</a>
+    <a href="#">Login</a>
+    <a href="#">Register</a>
+    <a href="/tardeBC/carrito/ver">Carrito</a>
+    <a href="/tardeBC/">Inicio</a>
+  </div>
+
+</header>
