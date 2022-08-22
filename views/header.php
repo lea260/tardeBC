cffe55b66f076d7d37d6cb328c55610fef3d1c28
GermanGuerrero26 <GermanGuerrero26>
2022-08-22 15:38:58 -0300

    Arregle listar

--- views/header.php
+++ views/header.php
@@ -1,1 +1,6 @@
-<header>Pagina</header>
+<nav>
+
+  <a href=" <?php echo constant('URL'); ?>" ">inicio</a>
+  <a href=" <?php echo constant('URL'); ?>articulos/listar" ">listar articulo</a>
+  <a href=" <?php echo constant('URL'); ?>articulos/listar" ">carrito</a>
+</nav>
