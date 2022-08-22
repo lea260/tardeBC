a1216170bbc8a29c6d7bc6695493348a2506f739
Maticerde <Maticerde>
2022-08-22 16:00:51 -0300

    maticerde

--- views/header.php
+++ views/header.php
@@ -1,1 +1,6 @@
-<header>header</header>
+<nav>
+  <a href="/tardeBC/"> Inicio </a>
+  <a href="/tardeBC/views/login/ingresar"> Login </a>
+  <a href="/tardeBC/views/Carrito/ver"> Carrito </a>
+  <a href="/tardeBC/views/articulos/listar"> articulos </a>
+</nav>
