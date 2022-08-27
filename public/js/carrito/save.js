f395acdadd6e52a34f96469543fc492648d285d3
Augusto Picardo <auguselo77@gmail.com>
2022-08-27 14:34:16 -0300

    Save

--- public/js/carrito/save.js
+++ public/js/carrito/save.js
@@ -6,27 +6,25 @@
     $("body").on("click", "#save", function () {
         let carrito = JSON.parse(localStorage.getItem("carrito"));
 
+
          $.ajax({
            url: `${url}carrito/save`,
            headers: headers,
            type: "POST",
-           data: JSON.stringify(carrito),
+           data: JSON.stringify({'lista':carrito}),
            dataType: "json",
+           success: function (data) {
+            localStorage.setItem("carrito", JSON.stringify([]));
+            
+            alert("Items added");
+          },
+          error: function (e) {
+            console.log(e.message);
+          },
          });
 
-        // $.ajax({
-        //   type: "POST",
-        //   dataType: "json",
-        //   url: `${url}carrito/save`,
-        //   data: JSON.stringify(carrito),
-        //   contentType: "application/json; charset=utf-8",
-        //   success: function (data) {
-        //     alert("Items added");
-        //   },
-        //   error: function (e) {
-        //     console.log(e.message);
-        //   },
-        // });
+
     }); //end body
   });
 })(jQuery);
+
