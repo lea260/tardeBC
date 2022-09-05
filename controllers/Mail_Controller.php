b4f81e9f0c332b0ebfb80317ffe5fe0711ae5469
Leandro <lcalvetti1@gmail.com>
2022-09-05 16:34:31 -0300

    enviar mail

---
+++ controllers/Mail_Controller.php
@@ -0,0 +1,39 @@
+ <?php
+
+require_once 'vendor/autoload.php';
+require "vendor/phpmailer/phpmailer/src/Exception.php";
+require "vendor/phpmailer/phpmailer/src/PHPMailer.php";
+require "vendor/phpmailer/phpmailer/src/SMTP.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+class Mail_Controller extends Controller
+{
+
+    public function enviar()
+    {
+        //$alumnos = $this->model->get();
+        //$this->view->alumnos = "cargado";
+        $correo  = constant('USER_DEST');
+        $mensaje = "mensaje desde app";
+
+        $oMail = new PHPMailer();
+        $oMail->isSMTP();
+        $oMail->Host       = "smtp.gmail.com";
+        $oMail->Port       = 587;
+        $oMail->SMTPSecure = "tls";
+        $oMail->SMTPAuth   = true;
+        $oMail->Username   = constant('USER_MAIL');
+        $oMail->Password   = constant('PWD_MAIL');
+        $oMail->setFrom(constant('USER_MAIL'), "programcionphp3bj");
+        $oMail->addAddress($correo, "Pedro");
+        $oMail->Subject = "Hola pepe el que pica";
+        $oMail->msgHTML($mensaje);
+
+        if (!$oMail->send()) {
+            echo $oMail->ErrorInfo;
+        }
+        //$this->view->render('mail/enviar');
+
+    }
+}
