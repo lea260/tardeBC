0e43bf50e699bdb3c3d1ace071712caa17cbd83b
Diego <diegoacunaowo@gmail.com>
2022-10-01 14:20:12 -0300

    Translate

--- views/header.php
+++ views/header.php
@@ -1,3 +1,8 @@
+<?php require_once 'traduccion/Translate.php';
+use \SimpleTranslation\Translate;
+Translate::__('home');?></a>
+?>
+
 <nav>
   <a href="<?php echo constant('URL'); ?>articulos/listar">Listar Articulos</a>
   <a href="<?php echo constant('URL'); ?>carrito/ver">Ver Carrito</a>
