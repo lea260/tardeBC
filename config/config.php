69c903baa34bd788e0f9615245049907f494320a
EON155 <clavijo02>
2022-10-03 16:07:10 -0200

    Se agregaron pedidos idiomas y headers

--- config/config.php
+++ config/config.php
@@ -1,12 +1,12 @@
 <?php
 
 //define('URL', 'http://localhost/mvc/');
-define('URL', 'http://' . $_SERVER['HTTP_HOST'] . 'tardeBC');
+define('URL', 'http://' . $_SERVER['HTTP_HOST'] . 'tardebc');
 
 //conexion a la base de datos
 define('HOST', 'localhost');
 define('PORT', '3307');
-define('DB', 'tardeBC');
+define('DB', 'tardebc');
 define('USER', 'usuario03');
 define('PASSWORD', "usuario03");
 define('CHARSET', 'utf8mb4');
