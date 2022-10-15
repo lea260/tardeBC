d5981a1517ecfab8f9e8567a5021a5e4cd0518e8
Leandro <lcalvetti1@gmail.com>
2022-10-15 14:42:37 -0300

    test token

--- controllers/Token_Controller.php
+++ controllers/Token_Controller.php
@@ -39,11 +39,12 @@ class Token_Controller extends Controller
     public function test()
     {
         try {
-            $tk   = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NjUyNTQ3OTcsImRhdGEiOnsidXN1YXJpb19pZCI6NSwicm9sIjoiYWRtaW4ifSwiaWlzIjoibG9jYWxob3N0In0.FpVxaiyaR7PaL3185wM8F1UuRMMeuflGi6LCmi-rpJI';
+            $tk   = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NjU4NTUwNzIsImRhdGEiOnsidXN1YXJpb19pZCI6NSwicm9sIjoiYWRtaW4ifSwiaWlzIjoibG9jYWxob3N0In0.oIXSEPV8BT5A7BVrO16LqinpiKBMSU3Ljl4S9PKG1sg';
             $data = Jwts::value($tk);
             print_r($data->data);
         } catch (Exception $th) {
             //throw $th;
+            echo "autorizacion invalida";
         }
 
     }
